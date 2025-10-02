7ec78d2898a019ed9d7ee617fb8d6493183fc563
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2025-10-02 16:09:52 +0800

    Create PMCinformation.tsx
    created 2nd section / PMC information component

---
+++ components/sections/about/PMCinformation.tsx
@@ -0,0 +1,9 @@
+export default function PMCInformation() {
+  return (
+    <section className="py-24">
+      <div className="">
+        <p>HELLO THIS IS MY 2ND SECTION</p>
+      </div>
+    </section>
+  );
+}
