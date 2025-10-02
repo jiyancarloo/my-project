fd7c2db31354167deb345e88fceca8142c9f155c
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2025-10-02 16:08:31 +0800

    Create footer.tsx
    created footer component

---
+++ components/ui/layout/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="w-auto h-80 flex items-center  bg-slate-200 p-5">
+        <div>
+          <p>Footer Section</p>
+        </div>
+      </div>
+    </>
+  );
+}
