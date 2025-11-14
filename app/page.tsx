e5f1755d1c16a37fda9730524b084dcd3399d21b
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2025-11-14 16:06:54 +0800

    Update page.tsx
    Imported 3 pages for render

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import { Button } from "@/components/ui/button";
 import Hero from "@/components/sections/hero";
 import PMCInformation from "@/components/sections/about/PMCinformation";
+import Contact from "@/components/sections/contact/contact";
+import Details from "@/components/sections/home/home";
+import Products from "@/components/sections/products/ProductSection";
 
 export default function Home() {
   return (
     <>
       <div>
         <Hero />
+        <Details />
         <PMCInformation />
+        <Products />
+        <Contact />
       </div>
     </>
   );
