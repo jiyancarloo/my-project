edd5501fe78acefbaac7a936565bb3c30c928df6
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2025-11-26 16:18:53 +0800

    Create Brands.tsx
    Rendered the carousel component in brands page

---
+++ components/sections/CarouselBrand/Brands.tsx
@@ -0,0 +1,13 @@
+import HospitalCarousel from "@/components/ui/CarouselHospital/Carousel";
+
+export default function Brands() {
+  return (
+    <>
+      <section className="py-4 min-h-[25vh] ">
+        <div className="py-4">
+          <HospitalCarousel />
+        </div>
+      </section>
+    </>
+  );
+}
