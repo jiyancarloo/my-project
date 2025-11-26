3496a6244a563ec4c1b5a41d282a2f95549a2cf8
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2025-11-26 16:19:17 +0800

    Update page.tsx
    added bg color for the whole page and imported brands page

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import PMCInformation from "@/components/sections/about/PMCinformation";
 import Contact from "@/components/sections/contact/contact";
 import Details from "@/components/sections/home/home";
 import Products from "@/components/sections/products/ProductSection";
+import Brands from "@/components/sections/CarouselBrand/Brands";
 
 export default function Home() {
   return (
     <>
-      <div className="">
+      <div className="bg-[#F4F6FB]">
         <Hero />
+        <Brands />
         <Details />
         <PMCInformation />
         <Products />
