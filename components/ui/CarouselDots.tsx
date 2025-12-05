7a79cd0030a52c8ec15225f2573625f2ffaad32a
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2025-12-05 16:28:47 +0800

    update
    Minimal changes to ui

---
+++ components/ui/CarouselDots.tsx
@@ -0,0 +1,36 @@
+import { EmblaCarouselType } from "embla-carousel";
+import { useEffect, useState } from "react";
+
+interface CarouselDotsProps {
+  emblaApi: EmblaCarouselType | undefined;
+}
+
+export default function CarouselDots({ emblaApi }: CarouselDotsProps) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    const onSelect = () => {
+      setSelectedIndex(emblaApi.selectedScrollSnap());
+    };
+
+    setScrollSnaps(emblaApi.scrollSnapList());
+    emblaApi.on("select", onSelect);
+    onSelect();
+  }, [emblaApi]);
+
+  return (
+    <div className="flex justify-center p-2 gap-2 mt-10 ">
+      {scrollSnaps.map((_, index) => (
+        <button
+          key={index}
+          onClick={() => emblaApi?.scrollTo(index)}
+          className={`h-0.5 w-8  transition-all duration-300 
+            ${selectedIndex === index ? "bg-third scale-110" : "bg-gray-300"}
+          `}></button>
+      ))}
+    </div>
+  );
+}
