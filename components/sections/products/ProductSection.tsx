30c8e2f2ab83285c3c3e4d831e89835e38f7c524
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2025-12-05 16:30:09 +0800

    Refactor ProductSection layout and component usage
    Replaces the main container div with a semantic section element, updates layout structure for improved readability, and renames Carousel to ProductCarousel for clarity.

--- components/sections/products/ProductSection.tsx
+++ components/sections/products/ProductSection.tsx
@@ -1,28 +1,28 @@
 "use client";
 import { useRef } from "react";
 import { Stethoscope } from "lucide-react";
-import Carousel from "./component/CarouselProduct";
+
+import ProductCarousel from "./component/CarouselProduct";
 
 export default function Products() {
   const sectionRef = useRef(null);
   return (
     <>
-      <div
-        id="product"
-        ref={sectionRef}
-        className="min-h-screen mx-auto relative ">
-        <div className="w-[80%] mx-auto flex flex-col p-6 gap-y-4 ">
-          <div className="p-6 flex items-start">
-            <span className="flex items-center gap-x-2  px-4 py-2 text-sm border border-black/30 rounded-full shadow-lg ">
-              <Stethoscope size={18} strokeWidth={1.8} />
-              Healthcare Solutions
-            </span>
-          </div>
-          <div>
-            <Carousel />
+      <section id="product" ref={sectionRef} className="min-h-screen relative">
+        <div className="py-20">
+          <div className="flex-1 py-20 justify-center ">
+            <div className="px-8 ">
+              <div className="flex items-start px-6">
+                <span className="flex items-center gap-x-2  px-4 py-2 text-sm border border-black/30 rounded-full shadow-lg ">
+                  <Stethoscope size={18} strokeWidth={1.8} />
+                  Healthcare Solutions
+                </span>
+              </div>
+            </div>
+            <ProductCarousel />
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
