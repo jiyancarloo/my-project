01e73a2c6e08d7764ae33be87701d0d578e1090a
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-07 11:06:25 +0800

    Update home.tsx
    Adjusted paddings and also the content and added corevalues component

--- components/sections/home/home.tsx
+++ components/sections/home/home.tsx
@@ -3,9 +3,8 @@
 import { useRef } from "react";
 import { HeartPulse } from "lucide-react";
 import CoreValues from "./component/CoreValues";
-import Carousel from "./component/HomeCarousel";
 import HomeCarousel from "./component/HomeCarousel";
-
+import Snowfall from "react-snowfall";
 export default function About() {
   const sectionRef = useRef(null);
   return (
@@ -13,40 +12,38 @@ export default function About() {
       <section
         ref={sectionRef}
         id="home"
-        className="min-h-screen mx-auto relative bg-white">
-        <div className="">
-          <div className="w-[80%] mx-auto flex flex-col items-center justify-center p-6 gap-y-4 py-20">
-            <div className="p-6">
-              <span className=" flex items-center gap-x-1  px-3 py-2 text-sm border border-black/30 rounded-full shadow-lg text-third ">
-                <HeartPulse size={18} strokeWidth={1.6} />
-                Who we are
+        className="min-h-screen mx-auto relative bg-[#F4F6FB] py-20">
+        <div className="w-[80%] mx-auto flex flex-col items-center justify-center p-6 gap-y-4 ">
+          <div className="p-6">
+            <span className=" flex items-center gap-x-1  px-3 py-2 text-sm border border-black/30 rounded-full shadow-lg text-third ">
+              <HeartPulse size={18} strokeWidth={1.6} />
+              Who we are
+            </span>
+          </div>
+          <Snowfall color="#162660" />
+
+          <div className="flex flex-col items-center gap-4">
+            <div className="">
+              <span className="text-4xl font-semibold text-center text-third">
+                Panamed Philippines Incorporation
               </span>
             </div>
 
-            <div className="flex flex-col items-center gap-4 text-third">
-              <div className="">
-                <span className="text-4xl font-semibold text-center">
-                  Panamed Philippines Incorporation
-                </span>
-              </div>
-
-              <div className="w-[60%]">
-                <p className="text-center text-lg/relaxed ">
-                  is a trusted professional organization specializing in the
-                  importation, marketing and distribution of high-quality
-                  medical devices. We have been a reliable partner in the
-                  healthcare industry for over 20 years, delivering exceptional
-                  service and innovative solutions to the Filipinos.
-                </p>
-              </div>
-            </div>
-
-            <div className="py-10">
-              <CoreValues />
+            <div className="w-[60%]">
+              <p className="text-center text-lg/relaxed ">
+                is a trusted professional organization specializing in the
+                importation, marketing and distribution of high-quality medical
+                devices. We have been a reliable partner in the healthcare
+                industry for over 20 years, delivering exceptional service and
+                innovative solutions to the Filipinos.
+              </p>
             </div>
           </div>
         </div>
-        <div className=" w-full    ">
+        <div className="py-20">
+          <CoreValues />
+        </div>
+        <div className="">
           <HomeCarousel />
         </div>
       </section>
