c69300cb361cffc00bb42c8d383ddc6b7b5881a6
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-07 11:08:22 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import Hero from "@/components/sections/hero";
 import Contact from "@/components/sections/contact/contact";
 import Products from "@/components/sections/products/ProductSection";
 import Brands from "@/components/sections/CarouselBrand/Brands";
 import About from "@/components/sections/home/home";
 import Awards from "@/components/sections/awards/Awards";
+
 import News from "@/components/sections/news/News";
 
 export default function Home() {
