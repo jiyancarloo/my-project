1f2d9ccdcae84ff990e9bbb3864f811e4cafaaf8
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-12 16:14:28 +0800

    Redesign News section with dynamic content and images
    Replaces static News section with a dynamic grid layout using data from the news array. Adds support for images, dates, titles, and excerpts, and improves styling with Next.js Image and Lucide HeartPulse icon.

--- components/sections/news/News.tsx
+++ components/sections/news/News.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useRef } from "react";
+import { news } from "@/app/data/news";
+import Image from "next/image";
+import { HeartPulse } from "lucide-react";
 
 export default function News() {
   const sectionRef = useRef(null);
@@ -9,11 +12,52 @@ export default function News() {
       <section
         id="news"
         ref={sectionRef}
-        className="min-h-screen mx-auto relative bg-[#F4F6FB] py-20">
-        <div>
-          <span>News Section</span>
+        className="min-h-screen relative py-25">
+        <div className="w-[90%] mx-auto">
+          <div className="flex justify-center">
+            <div className="">
+              <span className="flex items-center gap-x-1  px-3 py-2 text-sm border border-black/30 rounded-full shadow-lg text-third ">
+                <HeartPulse size={18} strokeWidth={1.6} />
+                News & Events
+              </span>
+            </div>
+          </div>
+          <div className="my-10">
+            <p className="text-center text-lg ">
+              Stay informed with the latest updates, announcements, and industry
+              insights from our team.
+            </p>
+          </div>
+
+          <div className="grid grid-cols-4 gap-6 mt-8">
+            {news.map((item, index) => (
+              <div className="" key={index}>
+                {item.images.map((image, index) => (
+                  <Image
+                    key={index}
+                    src={image.src}
+                    width={400}
+                    height={400}
+                    alt={image.alt}
+                    className="mb-3 rounded-lg object-fit-cover"
+                  />
+                ))}
+                <div className="mb-2">
+                  <span className="text-black/60 text-sm">{item.date}</span>
+                </div>
+                <div className="space-y-2">
+                  <div>
+                    <span className="text-lg font-semibold">{item.title}</span>
+                  </div>
+                  <div>
+                    <span>{item.exerpt}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
-    </> 
+    </>
   );
 }
