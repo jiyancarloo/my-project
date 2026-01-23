3ab9f62d8543cd095113ce73a7ac1b42328afcec
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-23 16:09:46 +0800

    Create CardInfo.ts
    Created an object file for mapping in the productCatalog section

---
+++ components/sections/ProductSection/data/CardInfo.ts
@@ -0,0 +1,57 @@
+export const cardInfo = [
+  {
+    title: "Anesthesia",
+    content:
+      "Experience a wide range of reliable medical equipment  built to empower professionals and strengthen healthcare delivery",
+    href: "/home_images/CARE.jpg",
+    alt: "Healthcare",
+  },
+  {
+    title: "Critical Care",
+    content: "Content",
+    href: "/home_images/CARE2.jpg",
+    alt: "Healthcare",
+  },
+  {
+    title: "Dialysis Products",
+    content: "Content",
+    href: "/about_images/Healthcare Professional.png",
+    alt: "Healthcare",
+  },
+  {
+    title: "Medical Surgical",
+    content: "Content",
+    href: "/home_images/CARE.jpg",
+    alt: "Healthcare",
+  },
+  {
+    title: "Protective Wear",
+    content: "Content",
+    href: "/home_images/CARE2.jpg",
+    alt: "Healthcare",
+  },
+  {
+    title: "Renal Care",
+    content: "Content",
+    href: "/about_images/Healthcare Professional.png",
+    alt: "Healthcare",
+  },
+  {
+    title: "Respiratory Care",
+    content: "Content",
+    href: "/home_images/CARE.jpg",
+    alt: "Healthcare",
+  },
+  {
+    title: "Waste Management",
+    content: "Content",
+    href: "/home_images/CARE2.jpg",
+    alt: "Healthcare",
+  },
+  {
+    title: "Urology",
+    content: "Content",
+    href: "/about_images/Healthcare Professional.png",
+    alt: "Healthcare",
+  },
+];
