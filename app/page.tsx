c1d4f62f18b957a9b826bc61a1c312c841a9c00b
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-23 16:03:23 +0800

    Update page.tsx
    Imported other sections in the page for rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,43 @@
 "use client";
 
 import Hero from "@/components/sections/hero";
-import Contact from "@/components/sections/contact/contactForm";
-import Products from "@/components/sections/products/ProductSection";
-import Brands from "@/components/sections/CarouselBrand/Brands";
-import About from "@/components/sections/home/home";
-import Awards from "@/components/sections/awards/Awards";
 
-import News from "@/components/sections/news/News";
+import Brands from "@/components/sections/CarouselSection/Brands";
+
+import About from "@/components/sections/HomeSection/AboutUs";
+import People from "@/components/sections/HomeSection/People";
+import CoreValues from "@/components/sections/HomeSection/CoreValues";
+
+import Services from "@/components/sections/ProductSection/Services";
+import ProductCarousel from "@/components/sections/ProductSection/CarouselProduct";
+import Flagship from "@/components/sections/ProductSection/FlagshipProduct";
+import TrustedPartners from "@/components/sections/ProductSection/TrustedPartners";
+
+import Contact from "@/components/sections/ContactSection/contactForm";
+
+import AwardsV2 from "@/components/sections/AwardsSection/AwardsV2";
+import News from "@/components/sections/NewsSection/News";
 
 export default function Home() {
   return (
     <>
       <div className="">
         <Hero />
+
         <Brands />
+
+        {/* ABOUT US */}
         <About />
-        <Products />
-        <Awards />
+        <People />
+        <CoreValues />
+
+        {/* SERVICES */}
+        <Services />
+        <ProductCarousel />
+        <Flagship />
+        <TrustedPartners />
+
+        <AwardsV2 />
         <News />
         <Contact />
       </div>
