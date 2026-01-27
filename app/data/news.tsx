f08edc5869e661e372240adf947f0d6a7d36c300
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-27 16:10:51 +0800

    Update news.tsx
    Updated news data in preparation for using in news page and news section

--- app/data/news.tsx
+++ app/data/news.tsx
@@ -5,32 +5,40 @@ export const news = [
     date: "March 17, 2025",
     exerpt:
       "Panamed Philippines, Inc. proudly participated in the 2025 Philippine Digestive Health Week Joint Annual Convention",
-    images: [{ src: "/home_images/CARE.jpg", alt: "care" }],
+    images: [
+      { src: "/news_images/News_1/article_cover.jpg", alt: "article_cover" },
+      { src: "/news_images/News_1/image_1.jpg", alt: "image1" },
+      { src: "/news_images/News_1/image_2.jpg", alt: "image2" },
+      { src: "/news_images/News_1/image_3.jpg", alt: "image3" },
+    ],
+    content:
+      "Panamed Philippines, Inc. proudly participated in the 2025 Philippine Digestive Health Week Joint Annual Convention, where we showcased our latest and most innovative digestive healthcare solutions. Held at Grand Hyatt Manila, BGC, this prestigious gathering brought together leading medical professionals, industry experts, and healthcare innovators to discuss advancements in digestive health.Throughout the event, our booth became a hub for insightful discussions as doctors and healthcare practitioners explored our cutting-edge products designed to enhance patient care. Our team was honored to connect with esteemed professionals, exchange knowledge, and highlight how our solutions contribute to the future of digestive healthcare. We extend our heartfelt gratitude to everyone who visited our booth and engaged with us. Your support and enthusiasm drive us to continue developing and providing top-tier medical solutions for the industry. We look forward to seeing you at future conventions as we remain committed to advancing healthcare innovations.",
   },
   {
-    title: "Supporting Healthcare and Serving the Community",
-    slug: "philippine-digestive-health-week",
+    title:
+      "PPI Corporate and Social Responsibility: Supporting Healthcare and Serving the Community",
+    slug: "supporting-healthcare",
     date: "January 21, 2025",
     exerpt:
       "As part of our CSR initiatives, PPI recently donated essential medical devices to San Juan Medical Center.",
-    images: [{ src: "/home_images/CARE.jpg", alt: "care" }],
+    images: [
+      { src: "/news_images/News_2/article_cover.jpg", alt: "article_cover" },
+      { src: "/news_images/News_2/image_1.jpg", alt: "image1" },
+      { src: "/news_images/News_2/image_2.jpg", alt: "image2" },
+    ],
   },
   {
     title:
       "Redefining the Future of Pulmonary Care: Our Participation at Bronc and Beyond 2025",
-    slug: "philippine-digestive-health-week",
-    date: "March 17, 2025",
+    slug: "redefining-pulmonary-care",
+    date: "March 19, 2025",
     exerpt:
       "The future of pulmonary care is here — and we’re proud to be part of the conversation.",
-    images: [{ src: "/home_images/CARE.jpg", alt: "care" }],
-  },
-  {
-    title:
-      "Redefining the Future of Pulmonary Care: Our Participation at Bronc and Beyond 2025",
-    slug: "philippine-digestive-health-week",
-    date: "March 17, 2025",
-    exerpt:
-      "The future of pulmonary care is here — and we’re proud to be part of the conversation.",
-    images: [{ src: "/home_images/CARE.jpg", alt: "care" }],
+    images: [
+      { src: "/news_images/News_3/article_cover.jpg", alt: "article_cover" },
+      { src: "/news_images/News_3/image_3.jpg", alt: "image3" },
+      { src: "/news_images/News_3/image_1.jpg", alt: "image1" },
+      { src: "/news_images/News_3/image_2.jpg", alt: "image2" },
+    ],
   },
 ];
