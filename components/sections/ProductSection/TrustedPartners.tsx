b1d45a3aab0de820e37301b1aeeef74dfe91359a
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-27 16:13:56 +0800

    Update TrustedPartners section layout and partners
    Added new partner images (EAMC, LCP, NKTI, PGH) to the partners list and adjusted the layout and styling of the TrustedPartners section for improved appearance and consistency. Image sizes and section structure were also updated.

--- components/sections/ProductSection/TrustedPartners.tsx
+++ components/sections/ProductSection/TrustedPartners.tsx
@@ -22,36 +22,50 @@ export default function TrustedPartners() {
       href: "/partner_images/PHC.png",
       alt: "phc",
     },
+    {
+      href: "/partner_images/EAMC.png",
+      alt: "eamc",
+    },
+    {
+      href: "/partner_images/LCP.png",
+      alt: "lcp",
+    },
+    {
+      href: "/partner_images/NKTI.png",
+      alt: "nkti",
+    },
+    {
+      href: "/partner_images/PGH.png",
+      alt: "pgh",
+    },
+    {
+      href: "/partner_images/PHC.png",
+      alt: "phc",
+    },
   ];
   return (
     <>
-      <section className="bg-[#F4F6FB]">
-        <div className="mx-40 py-20 ">
-          <div className="mx-8 w-fit flex items-center gap-2">
-            <div className="w-8 h-px bg-third rounded-full" />
-            <span className="text-black/60 uppercase tracking-wide whitespace-nowrap">
-              TRUSTED PARTNERS
-            </span>
-          </div>
-          <div className="grid grid-cols-1 lg:grid-cols-[2fr_2fr] max-w-8xl place-items-center ">
-            <div className="p-8 space-y-10">
+      <section className="   ">
+        <div className="mx-30 py-20">
+          <div className="place-items-center space-y-14">
+            <div>
               <article className="text-pretty">
-                <span className="text-4xl text-third">
+                <span className="text-xl text-third">
                   Working alongside trusted healthcare institutions nationwide.
                 </span>
               </article>
             </div>
-            <div className="flex itpartner">
+            <div className="flex gap-12">
               {partners.map((partner, index) => (
                 <div
                   key={index}
-                  className="w-34 h-34 flex items-center justify-center border p-4">
+                  className=" inline-flex items-center justify-center rounded-lg ">
                   <Image
                     src={partner.href}
                     alt={partner.alt}
-                    width={300}
-                    height={300}
-                    className="object-contain grayscale "
+                    width={100}
+                    height={100}
+                    className="object-cover grayscale"
                   />
                 </div>
               ))}
