01edf51945784418de112812cc6416a99b7f32ef
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-28 16:13:55 +0800

    Refactor news article page layout and content rendering
    Updated the news article page to improve layout structure and content rendering. Article content is now rendered dynamically from a block-based structure, supporting both paragraphs and images. Adjusted styling for better readability and maintainability.

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -14,40 +14,70 @@ export default async function NewsArticle({ params }: Props) {
 
   return (
     <>
-      <section className="bg-[#F4F6FB] min-h-screen">
-        <div>
-          <article className="max-w-4xl mx-auto p-8 text-pretty space-y-6">
-            <span className="text-black/60 uppercase tracking-wide whitespace-nowrap block">
-              {article.date}
-            </span>
-
-            <div className="flex gap-4 ">
-              <div className="bg-third w-1" />
-              <h1 className="text-3xl block">{article.title}</h1>
-            </div>
-
-            <div className="bg-red-50">
-              <Image
-                src={article.images[0].src}
-                alt={article.images[0].alt}
-                width={500}
-                height={400}
-                className="object-cover w-full"
-              />
-            </div>
-
-            <div className="prose">{article.exerpt}</div>
-            <p className="">{article.content}</p>
-            <Link
-              href="/#news"
-              className="inline-flex items-center gap-1 text-third font-medium group mt-2">
-              Home
-              <span className="transform transition-transform duration-300 group-hover:translate-x-1">
-                →
-              </span>
-            </Link>
-            <RelatedNews currentSlug={slug} />
+      <section className="min-h-screen bg-[#F4F6FB]  ">
+        <div className="max-w-4xl mx-auto p-8 text-pretty space-y-6   ">
+          {/* <div className="bg-red-50 w-full relative aspect-video ">
+            <Image
+              src={article.coverImage.src}
+              alt={article.coverImage.alt}
+              fill
+              className="object-cover object-bottom rounded-lg"
+            />
+          </div> */}
+          <article className="flex gap-4">
+            <div className="bg-third w-1" />
+            <h1 className="text-3xl block ">{article.title}</h1>
           </article>
+
+          <span className="text-black/60 uppercase tracking-wide whitespace-nowrap block prose prose-md">
+            {article.date}
+          </span>
+
+          <article className="prose prose-md prose-stone max-w-none">
+            <p>{article.exerpt}</p>
+          </article>
+
+          <article className="prose max-w-none">
+            {article.content.map((block, index) => {
+              if (block.type === "paragraph") {
+                return (
+                  <p key={index} className="">
+                    {block.text}
+                  </p>
+                );
+              }
+
+              if (block.type === "image") {
+                const img = article.images[block.imageIndex!];
+                return (
+                  <div
+                    className="w-full max-w-5xl overflow-hidden "
+                    key={index}>
+                    <div className="relative w-full aspect-video">
+                      <Image
+                        src={img.src}
+                        alt={img.alt}
+                        fill
+                        priority
+                        className="object-cover"
+                      />
+                    </div>
+                  </div>
+                );
+              }
+            })}
+          </article>
+          <Link
+            href="/#news"
+            className="inline-flex items-center gap-1 text-third font-medium group mt-2">
+            Home
+            <span className="transform transition-transform duration-300 group-hover:translate-x-1">
+              →
+            </span>
+          </Link>
+          <div>
+            <RelatedNews currentSlug={slug} />
+          </div>
         </div>
       </section>
     </>
