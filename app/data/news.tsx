56012586f877bdbd989de2c11a7f406eea4126e0
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-29 16:30:21 +0800

    Update news.tsx

--- app/data/news.tsx
+++ app/data/news.tsx
@@ -36,11 +36,14 @@ export const news: NewsItem[] = [
         type: "paragraph",
         text: "We showcased our latest and most innovative digestive healthcare solutions. Held at Grand Hyatt Manila, BGC, this prestigious gathering brought together leading medical professionals, industry experts, and healthcare innovators to discuss advancements in digestive health.Throughout the event, our booth became a hub for insightful discussions as doctors and healthcare practitioners explored our cutting-edge products designed to enhance patient care. ",
       },
-
+      /* {
+        type: "image",
+        imageIndex: 1,
+      },
       {
         type: "image",
-        imageIndex: 0,
-      },
+        imageIndex: 2,
+      }, */
       {
         type: "paragraph",
         text: "Our team was honored to connect with esteemed professionals, exchange knowledge, and highlight how our solutions contribute to the future of digestive healthcare. We extend our heartfelt gratitude to everyone who visited our booth and engaged with us. Your support and enthusiasm drive us to continue developing and providing top-tier medical solutions for the industry. We look forward to seeing you at future conventions as we remain committed to advancing healthcare innovations. ",
@@ -69,10 +72,10 @@ export const news: NewsItem[] = [
         type: "paragraph",
         text: "Aiming to enhance healthcare services and support those in need. The donation was gratefully received by Dr. Joselito P. Lonzame, Assistant Medical Director, and Dra. Elenita Fernando, Chief of Clinics. At PPI, we are dedicated to making a positive impact, not only through our products but also through actions that truly make a difference in the community. ",
       },
-      {
+      /*    {
         type: "image",
         imageIndex: 0,
-      },
+      }, */
 
       {
         type: "paragraph",
@@ -88,7 +91,7 @@ export const news: NewsItem[] = [
     exerpt:
       "The future of pulmonary care is here — and we’re proud to be part of the conversation.",
     coverImage: {
-      src: "/news_images/News_3/article_cover.jpg",
+      src: "/news_images/News_3/image_1.jpg",
       alt: "article_cover",
     },
 
@@ -104,10 +107,11 @@ export const news: NewsItem[] = [
         text: "Our team recently took part in this premier event that brought together respiratory experts, clinicians, and industry innovators dedicated to advancing pulmonary medicine and patient care. At our booth, we showcased our latest respiratory and critical care solutions, designed to meet the evolving demands of modern healthcare. Attendees had the opportunity to explore our cutting-edge technologies and engage directly with our team of experts, who shared insights into how our products are shaping the next generation of respiratory support. ",
       },
 
-      {
+      /*  {
         type: "image",
         imageIndex: 2,
-      },
+      }, */
+
       {
         type: "paragraph",
         text: "We extend our heartfelt thanks to everyone who stopped by our booth—your enthusiasm and interest drive us to continue innovating and improving. As we move forward, we remain steadfast in our commitment to delivering advanced healthcare solutions that empower medical professionals and improve the quality of life for patients everywhere.",
