4b7c804d3e11663a8ac17dccd2ec59354b49f817
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-01-30 16:34:04 +0800

    Update contactForm.tsx
    Refactored contact form, removed white background and reduce padding top and bottom

--- components/sections/ContactSection/contactForm.tsx
+++ components/sections/ContactSection/contactForm.tsx
@@ -6,12 +6,12 @@ export default function Contact() {
   const sectionRef = useRef(null);
   return (
     <>
-      <section id="contact" ref={sectionRef} className=" py-30 bg-[#F4F6FB]">
+      <section id="contact" ref={sectionRef} className=" py-20 bg-[#F4F6FB]">
         <div className="mx-30">
           {/* 1st Column */}
-          <div className="grid grid-cols-1 lg:grid-cols-[2fr_2fr] items-start">
+          <div className="grid grid-cols-1 lg:grid-cols-[2fr_2fr] items-center">
             {/* Left Column */}
-            <div className="p-8">
+            <div className="p-8 ">
               <article className="text-pretty">
                 <p className="text-4xl   text-third">
                   Do you have any inquiries about billing, sales, accounting or
@@ -21,13 +21,9 @@ export default function Contact() {
             </div>
 
             {/* Right Column */}
-            <div className="w-full max-w-2xl justify-self-center border rounded-lg bg-white shadow-lg flex flex-col ">
-              <div className="px-8 pt-8">
-                <h2 className="text-3xl  text-third">Get in Touch</h2>
-              </div>
-
+            <div className="w-full max-w-2xl justify-self-center  flex flex-col ">
               <div className="flex-1 grid grid-cols-1 md:grid-cols-4 gap-6 p-8">
-                <div className="md:col-span-2 flex flex-col gap-1">
+                <div className="md:col-span-4 flex flex-col gap-1">
                   <label className="text-sm font-medium text-gray-600">
                     First Name
                   </label>
@@ -36,12 +32,6 @@ export default function Contact() {
 
                 <div className="md:col-span-2 flex flex-col gap-1">
                   <label className="text-sm font-medium text-gray-600">
-                    Last Name
-                  </label>
-                  <input className="rounded-md border px-3 py-2 text-sm focus:ring-2 focus:ring-blue-500" />
-                </div>
-                <div className="md:col-span-4 flex flex-col gap-1">
-                  <label className="text-sm font-medium text-gray-600">
                     Email
                   </label>
                   <input
@@ -49,7 +39,7 @@ export default function Contact() {
                     className="rounded-md border px-3 py-2 text-sm focus:ring-2 focus:ring-blue-500"
                   />
                 </div>
-                <div className="md:col-span-4 flex flex-col gap-1">
+                <div className="md:col-span-2 flex flex-col gap-1">
                   <label className="text-sm font-medium text-gray-600">
                     Phone Number
                   </label>
@@ -63,7 +53,7 @@ export default function Contact() {
                     Message
                   </label>
                   <textarea
-                    rows={8}
+                    rows={2}
                     cols={80}
                     placeholder="Enter your message here..."
                     className="rounded-md border px-3 py-2 text-sm focus:ring-2 focus:ring-blue-500"
