ee8d51697317e3425fd3069ce5f82f9c140ef094
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-02-04 16:34:24 +0800

    Make Flagship product layout responsive
    Adjust responsive classes in FlagshipProduct.tsx: move outer spacing to a lg-only container, change grid to lg:grid-cols-2 so it stacks on smaller screens, and update typography and button padding/text sizes with mobile-first (text-xl -> text-xl lg:text-4xl, p text-base lg:text-lg, and button px/py + text-base lg:text-lg). These changes improve spacing and readability across breakpoints.

--- components/sections/ProductSection/FlagshipProduct.tsx
+++ components/sections/ProductSection/FlagshipProduct.tsx
@@ -2,22 +2,24 @@ import Image from "next/image";
 
 export default function Flagship() {
   return (
-    <div className="mx-30 py-30">
+    <div className="">
       {/* Two-column grid */}
-      <div className="">
-        <div className="grid grid-cols-2 items-center ">
+      <div className="lg:mx-30 lg:py-30 ">
+        <div className="grid lg:grid-cols-2 items-center ">
           {/* LEFT COLUMN – TEXT */}
           <div className="p-8 space-y-6">
-            <span className="text-white text-4xl  block">
+            <span className="text-white text-xl lg:text-4xl  block">
               <span className="text-third">Flagship Product Goes here</span>
             </span>
 
-            <p className="text-black/60 text-lg/relaxed">Item Description</p>
+            <p className="text-black/60 text-base lg:text-lg/relaxed">
+              Item Description
+            </p>
             <div className="flex items-center pt-2">
               <div className="group inline-block relative pt-2 z-20 ">
                 <button
-                  className="relative overflow-hidden p-8 py-4 w-fit rounded-full border border-black/50  text-0
-                transition-colors duration-500 text-lg text-black">
+                  className="relative overflow-hidden px-4 lg:p-8 py-2 lg:py-4 w-fit rounded-full border border-black/50  text-0
+                transition-colors duration-500 text-base lg:text-lg text-black">
                   <span className="absolute inset-0 bg-third -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out rounded-lg"></span>
                   <span className="relative z-10 group-hover:text-white transition-colors duration-500 ">
                     Buy Now
