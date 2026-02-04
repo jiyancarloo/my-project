e48c84506c77fa3483232486bf7735e2ffab884c
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-02-04 16:30:47 +0800

    Update hero.tsx
    started working on resizing image in hero depending on viewport and also adjusted font sizing when it comes to mobile and desktop view.

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,16 +1,31 @@
 "use client";
 import gsap from "gsap";
 import { useRef } from "react";
-import React from "react";
-import Image from "next/image";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import React from "react";
+import Image from "next/image";
 import { Star } from "lucide-react";
+import useEmblaCarousel from "embla-carousel-react";
+import AutoScroll from "embla-carousel-auto-scroll";
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
-
 export default function Hero() {
+  const [emblaRef] = useEmblaCarousel({ loop: true, dragFree: false }, [
+    AutoScroll({ speed: 1, stopOnInteraction: false, startDelay: 100 }),
+  ]);
+  const brandInfo = [
+    { src: "/brand_images/Endosenz.png", alt: "endo" },
+    { src: "/brand_images/Pana-Vac_Logo.png", alt: "pana" },
+    { src: "/brand_images/Sedasenz_Logo.png", alt: "seda" },
+    { src: "/brand_images/Unigard_Logo.png", alt: "uni" },
+    { src: "/brand_images/Unimex_Logo.png", alt: "uni" },
+    { src: "/brand_images/anchor_Logo.png", alt: "anchor" },
+    { src: "/brand_images/Brielle_Logo.png", alt: "bri" },
+    { src: "/brand_images/Universal_Logo.png", alt: "uni" },
+    { src: "/brand_images/Urosenz_Logo.png", alt: "uro" },
+  ];
   const scrollToSection = (sectionId: string) => {
     gsap.to(window, {
       duration: 1,
@@ -20,11 +35,9 @@ export default function Hero() {
   };
   const heroRef = useRef(null);
   return (
-    <section
-      ref={heroRef}
-      className="min-h-[100vh]flex items-center relative z-10 ">
+    <section ref={heroRef} className="relative z-10">
       {/* Container */}
-      <div className="relative h-[80vh] w-full flex flex-col items-center justify-center overflow-hidden border-b-2">
+      <div className="relative min-h-screen lg:min-h-[80vh] w-full flex flex-col  justify-center overflow-hidden border-b-2">
         {/* Background image */}
         <Image
           src="/hero_image/hero1.jpg"
@@ -34,56 +47,49 @@ export default function Hero() {
           sizes="(max-width: 768px) 100vw, 100vw"
           priority
         />
-
         <div className="absolute inset-0 bg-black/50 -z-10"></div>
+        {/* <div className="flex items-center gap-2"> <div className=" border-white/60 w-8 h-px border"></div> <span className="text-sm text-blue-200 font-semibold"> PREMIUM SOLUTIONS </span> </div> */}
+        <div className="max-w-7xl flex flex-col lg:items-center gap-6 md:gap-8 relative z-20 mx-auto px-4">
+          {/* Container for text header */}
+          <div className="max-w-5xl text-left lg:text-center space-y-3">
+            <h1 className="text-3xl md:text-4xl lg:text-5xl/16 font-semibold text-white">
+              Delivering quality healthcare solutions with{" "}
+              <span className="text-blue-200">integrity and innovation</span>
+            </h1>
+            <p className="text-md md:text-md lg:text-xl text-white ">
+              your trusted partner in advanced medical equipment and healthcare
+              <br /> supply solutions for more than
+              <span className="text-blue-200"> 20 years</span>
+            </p>
+          </div>
 
-        {/* CENTER CONTENT */}
-        {/*  <div className="flex items-center gap-2">
-          <div className=" border-white/60 w-8 h-px border"></div>
-          <span className="text-sm text-blue-200 font-semibold">
-            PREMIUM SOLUTIONS
-          </span>
-        </div> */}
-
-        <div className="relative z-20 flex flex-col justify-center items-center gap-y-8  text-center  ">
-          <h1 className="text-6xl/17 font-semibold text-white">
-            Delivering quality healthcare solutions <br />
-            with <span className="text-blue-200">integrity and innovation</span>
-          </h1>
-
-          <p className="text-2xl text-white ">
-            your trusted partner in advanced medical equipment and healthcare
-            <br /> supply solutions for more than
-            <span className="text-blue-200"> 20 years</span>{" "}
-          </p>
-
-          <div className="flex items-center gap-2">
+          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center gap-4">
             <div className="flex -space-x-2 overflow-hidden">
               <img
                 src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt="1"
-                className="inline-block size-10 rounded-full ring-2 ring-gray-900 outline -outline-offset-1 outline-white/10"
+                className="inline-block size-9 lg:size-10 rounded-full ring-2 ring-gray-900 outline -outline-offset-1 outline-white/10"
               />
               <img
                 src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt="2"
-                className="inline-block size-10 rounded-full ring-2 ring-gray-900 outline -outline-offset-1 outline-white/10"
+                className="inline-block size-9 lg:size-10 rounded-full ring-2 ring-gray-900 outline -outline-offset-1 outline-white/10"
               />
               <img
                 src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
                 alt="3"
-                className="inline-block size-10 rounded-full ring-2 ring-gray-900 outline -outline-offset-1 outline-white/10"
+                className="inline-block size-9 lg:size-10 rounded-full ring-2 ring-gray-900 outline -outline-offset-1 outline-white/10"
               />
               <img
                 src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                 alt="4"
-                className="inline-block size-10 rounded-full ring-2 ring-gray-900 outline -outline-offset-1 outline-white/10"
+                className="inline-block size-9 lg:size-10 rounded-full ring-2 ring-gray-900 outline -outline-offset-1 outline-white/10"
               />
             </div>
             <div className="flex flex-col gap-1">
               <div className="flex items-start ">
                 {Array.from({ length: 5 }).map((_, rate) => (
-                  <Star key={rate} size={18} className="fill-yellow-400 " />
+                  <Star key={rate} className="fill-yellow-400 " size={18} />
                 ))}
               </div>
               <span className="text-xs text-white">
@@ -96,8 +102,8 @@ export default function Hero() {
             <div className="group inline-block relative pt-2 z-20 ">
               <button
                 onClick={() => scrollToSection("#product")}
-                className="relative overflow-hidden p-8 py-4 w-fit rounded-full border border-white/50  text-white  transition-colors duration-500 text-lg">
-                <span className="absolute inset-0 bg-blue-200 -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out rounded-lg"></span>
+                className="relative overflow-hidden px-4 lg:p-8 py-3 lg:py-4 w-fit rounded-full border border-white/50 text-white lg:text-lg transition-colors duration-500 ">
+                <span className="absolute inset-0 bg-blue-200 -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-in-out rounded-lg" />
                 <span className="relative z-10 group-hover:text-primary-600 transition-colors duration-500 ">
                   Explore Our Solutions
                 </span>
@@ -106,6 +112,28 @@ export default function Hero() {
           </div>
         </div>
       </div>
+
+      {/* Brand Carousel */}
+      <section className="lg:py-4 sm:py-2">
+        <div className="overflow-hidden pointer-events-none" ref={emblaRef}>
+          <div className="flex gap-2">
+            {brandInfo.map((item, index) => (
+              <div key={index} className="flex-[0_0_16%]">
+                <div className="h-40 flex items-center justify-center">
+                  <div className="relative w-36 h-8">
+                    <Image
+                      src={item.src}
+                      alt={item.alt}
+                      fill
+                      className="filter grayscale"
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </section>
   );
 }
