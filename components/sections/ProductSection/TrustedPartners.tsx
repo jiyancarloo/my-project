48a7af6296576419ec50d0a2ea017adbea7fa2d1
Gian Carlo Catalan <166023368+jiyancarloo@users.noreply.github.com>
2026-02-06 16:33:06 +0800

    Update TrustedPartners.tsx

--- components/sections/ProductSection/TrustedPartners.tsx
+++ components/sections/ProductSection/TrustedPartners.tsx
@@ -22,50 +22,32 @@ export default function TrustedPartners() {
       href: "/partner_images/PHC.png",
       alt: "phc",
     },
-    {
-      href: "/partner_images/EAMC.png",
-      alt: "eamc",
-    },
-    {
-      href: "/partner_images/LCP.png",
-      alt: "lcp",
-    },
-    {
-      href: "/partner_images/NKTI.png",
-      alt: "nkti",
-    },
-    {
-      href: "/partner_images/PGH.png",
-      alt: "pgh",
-    },
   ];
   return (
     <>
-      <section>
-        <div className="px-4 py-12 sm:px-8 lg:px-30 lg:py-20">
-          <div className="space-y-10 text-center">
-            {/* Heading */}
-            <article className="text-pretty max-w-2xl mx-auto">
-              <span className="text-lg sm:text-xl lg:text-2xl text-third">
-                Working alongside trusted healthcare institutions nationwide.
-              </span>
-            </article>
+      <section className="px-4 py-12 sm:px-8 lg:px-30 lg:py-20">
+        <div className="grid lg:grid-cols-2 place-items-center ">
+          {/* Heading */}
+          <article className="text-pretty max-w-2xl ">
+            <span className="text-lg sm:text-xl lg:text-2xl text-third">
+              Working alongside trusted healthcare institutions nationwide.
+            </span>
+          </article>
 
-            {/* Logos */}
-            <div className="flex flex-wrap lg:flex-nowrap justify-center gap-6 sm:gap-8 lg:gap-12">
-              {partners.map((partner, index) => (
-                <div
-                  key={index}
-                  className="w-20 h-20 sm:w-24 sm:h-24 lg:w-24 lg:h-24 relative shrink-0">
-                  <Image
-                    src={partner.href}
-                    alt={partner.alt}
-                    fill
-                    className="object-contain grayscale hover:grayscale-0 hover:scale-120 transition duration-300  "
-                  />
-                </div>
-              ))}
-            </div>
+          {/* Logos */}
+          <div className="flex flex-wrap lg:flex-nowrap  gap-6 sm:gap-8 lg:gap-12">
+            {partners.map((partner, index) => (
+              <div
+                key={index}
+                className="w-20 h-20 sm:w-24 sm:h-24 lg:w-24 lg:h-24 relative shrink-0">
+                <Image
+                  src={partner.href}
+                  alt={partner.alt}
+                  fill
+                  className="object-contain grayscale hover:grayscale-0 hover:scale-120 transition duration-300  "
+                />
+              </div>
+            ))}
           </div>
         </div>
       </section>
